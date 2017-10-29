ffb890ca75a8edd982a75f8e3679e740723c3c38
shaass <33196524+shaass@users.noreply.github.com>
2017-10-29 16:43:54 +0100

    n

--- Sandragil/Sandragil/MainWindow.Designer.cs
+++ Sandragil/Sandragil/MainWindow.Designer.cs
@@ -45,9 +45,10 @@
             // 
             // wizzard
             // 
-            this.wizzard.Location = new System.Drawing.Point(127, 138);
+            this.wizzard.Location = new System.Drawing.Point(339, 329);
+            this.wizzard.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
             this.wizzard.Name = "wizzard";
-            this.wizzard.Size = new System.Drawing.Size(75, 23);
+            this.wizzard.Size = new System.Drawing.Size(200, 55);
             this.wizzard.TabIndex = 0;
             this.wizzard.Text = "start wizzard";
             this.wizzard.UseVisualStyleBackColor = true;
@@ -55,9 +56,10 @@
             // 
             // herpes
             // 
-            this.herpes.Location = new System.Drawing.Point(284, 138);
+            this.herpes.Location = new System.Drawing.Point(757, 329);
+            this.herpes.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
             this.herpes.Name = "herpes";
-            this.herpes.Size = new System.Drawing.Size(75, 23);
+            this.herpes.Size = new System.Drawing.Size(200, 55);
             this.herpes.TabIndex = 1;
             this.herpes.Text = "start herpes";
             this.herpes.UseVisualStyleBackColor = true;
@@ -65,9 +67,10 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(474, 138);
+            this.button3.Location = new System.Drawing.Point(1264, 329);
+            this.button3.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.Size = new System.Drawing.Size(200, 55);
             this.button3.TabIndex = 2;
             this.button3.Text = "button3";
             this.button3.UseVisualStyleBackColor = true;
@@ -75,29 +78,31 @@
             // LabelHerpes
             // 
             this.LabelHerpes.AutoSize = true;
-            this.LabelHerpes.Location = new System.Drawing.Point(289, 71);
-            this.LabelHerpes.Margin = new System.Windows.Forms.Padding(1, 0, 1, 0);
+            this.LabelHerpes.Location = new System.Drawing.Point(771, 169);
             this.LabelHerpes.Name = "LabelHerpes";
-            this.LabelHerpes.Size = new System.Drawing.Size(64, 13);
+            this.LabelHerpes.Size = new System.Drawing.Size(169, 32);
             this.LabelHerpes.TabIndex = 3;
             this.LabelHerpes.Text = "Play Herpes";
             // 
             // LabelWizzard
             // 
             this.LabelWizzard.AutoSize = true;
-            this.LabelWizzard.Location = new System.Drawing.Point(127, 70);
+            this.LabelWizzard.Location = new System.Drawing.Point(339, 167);
+            this.LabelWizzard.Margin = new System.Windows.Forms.Padding(8, 0, 8, 0);
             this.LabelWizzard.Name = "LabelWizzard";
-            this.LabelWizzard.Size = new System.Drawing.Size(68, 13);
+            this.LabelWizzard.Size = new System.Drawing.Size(180, 32);
             this.LabelWizzard.TabIndex = 4;
             this.LabelWizzard.Text = "Play Wizzard";
             // 
             // menuStrip1
             // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(40, 40);
             this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.optionsToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(676, 24);
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(16, 5, 0, 5);
+            this.menuStrip1.Size = new System.Drawing.Size(1803, 55);
             this.menuStrip1.TabIndex = 5;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -106,7 +111,7 @@
             this.optionsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.userToolStripMenuItem});
             this.optionsToolStripMenuItem.Name = "optionsToolStripMenuItem";
-            this.optionsToolStripMenuItem.Size = new System.Drawing.Size(61, 20);
+            this.optionsToolStripMenuItem.Size = new System.Drawing.Size(136, 45);
             this.optionsToolStripMenuItem.Text = "Options";
             // 
             // userToolStripMenuItem
@@ -117,38 +122,38 @@
             this.changePWToolStripMenuItem,
             this.changeUNToolStripMenuItem});
             this.userToolStripMenuItem.Name = "userToolStripMenuItem";
-            this.userToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.userToolStripMenuItem.Size = new System.Drawing.Size(326, 46);
             this.userToolStripMenuItem.Text = "User";
             // 
             // addToolStripMenuItem
             // 
             this.addToolStripMenuItem.Name = "addToolStripMenuItem";
-            this.addToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.addToolStripMenuItem.Size = new System.Drawing.Size(326, 46);
             this.addToolStripMenuItem.Text = "Add";
             // 
             // removeToolStripMenuItem
             // 
             this.removeToolStripMenuItem.Name = "removeToolStripMenuItem";
-            this.removeToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.removeToolStripMenuItem.Size = new System.Drawing.Size(326, 46);
             this.removeToolStripMenuItem.Text = "Remove";
             // 
             // changePWToolStripMenuItem
             // 
             this.changePWToolStripMenuItem.Name = "changePWToolStripMenuItem";
-            this.changePWToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.changePWToolStripMenuItem.Size = new System.Drawing.Size(326, 46);
             this.changePWToolStripMenuItem.Text = "Change PW";
             // 
             // changeUNToolStripMenuItem
             // 
             this.changeUNToolStripMenuItem.Name = "changeUNToolStripMenuItem";
-            this.changeUNToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.changeUNToolStripMenuItem.Size = new System.Drawing.Size(326, 46);
             this.changeUNToolStripMenuItem.Text = "Change UN";
             // 
             // MainWindow
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(676, 292);
+            this.ClientSize = new System.Drawing.Size(1803, 696);
             this.Controls.Add(this.LabelWizzard);
             this.Controls.Add(this.LabelHerpes);
             this.Controls.Add(this.button3);
@@ -156,6 +161,7 @@
             this.Controls.Add(this.wizzard);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
             this.Name = "MainWindow";
             this.Text = "MainWindow";
             this.menuStrip1.ResumeLayout(false);
